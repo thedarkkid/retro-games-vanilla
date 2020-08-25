b4ba63a999998ba62fdca0a1af08f74f7bcdc54a
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-08-25 16:18:57 +0400

    initial code arch

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devtool: 'inline-source-map',
+    target: 'node',
+    watch: true,
+    mode: 'development',
+    resolve: {
+        alias: {
+        },
+        extensions: ['.ts', '.js', '.tsx']
+    },
+    watchOptions: {
+        aggregateTimeout: 600,
+        ignored: /node_modules/
+    },
+    entry: {
+        app:  './src/app.ts',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'app.js'
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/static/index.html",
+            filename: "./src/static/index.html",
+            excludeChunks: [ 'app' ]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                // Transpiles ES6-8 into ES5
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                // Loads the javacript into html template provided.
+                // Entry point is set below in HtmlWebPackPlugin in Plugins
+                test: /\.html$/,
+                use: [{loader: "html-loader"}]
+            }
+        ]
+    }
+};
+
+
+module.exports = [appConfig, highwayConfig];
